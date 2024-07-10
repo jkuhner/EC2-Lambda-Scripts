b7e2841aaa2d111ed41091e1a37ec4389e971570
Jeff Kuhner <168482550+jkuhner@users.noreply.github.com>
2024-07-10 07:53:32 -0700

    Add files via upload

---
+++ Power_Up_Mon-Sat_21-0630.py
@@ -0,0 +1,39 @@
+import boto3
+import logging
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#define the connection
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    # Use the filter() method of the instances collection to retrieve
+    # all running EC2 instances.
+    filters = [{
+            'Name': 'tag:Power',
+            'Values': ['Mon-Sat-21:00-06:30']
+        },
+#        {
+#            'Name': 'instance-state-name', 
+#            'Values': ['stopped']
+#        }
+    ]
+    
+    #filter the instances
+    instances = ec2.instances.filter(Filters=filters)
+
+    #locate all instances
+    RunningInstances = [instance.id for instance in instances]
+    
+    #print the instances for logging purposes
+    #print RunningInstances 
+    
+    #make sure there are actually instances to Power on. 
+    if len(RunningInstances) > 0:
+        #Power on
+        poweringOn = ec2.instances.filter(InstanceIds=RunningInstances).start()
+        print (poweringOn)
+    else:
+        print ("Nothing to see here") 
